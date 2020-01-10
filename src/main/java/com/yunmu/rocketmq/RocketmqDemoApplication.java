afb8acd245726aa4d9211c99f1e5db3079bcec0f
mfXing <xingmingfeng@chainup.com>
2020-01-10 15:44:03 +0800

    add notes

--- src/main/java/com/yunmu/rocketmq/RocketmqDemoApplication.java
+++ src/main/java/com/yunmu/rocketmq/RocketmqDemoApplication.java
@@ -3,6 +3,9 @@ package com.yunmu.rocketmq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author mfXing
+ */
 @SpringBootApplication
 public class RocketmqDemoApplication {
 
