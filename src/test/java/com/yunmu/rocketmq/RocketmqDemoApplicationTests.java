4a933c8563a9528a0c604eb13e6c107ac448aaf0
mfXing <xingmingfeng@chainup.com>
2020-01-10 15:37:21 +0800

    git init

---
+++ src/test/java/com/yunmu/rocketmq/RocketmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunmu.rocketmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
